6eeab9e950be9f32f87af8407e334546f45e6ad4
i1006 <arjit.srivastava@innovaccer.com>
2021-07-16 13:25:03 +0530

    removing secret from exposing up

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&3xn+b#33vuz9949a^lc+6lc!pmgg!l%#jtn*g8u+o0c%7z@&^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
